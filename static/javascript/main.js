3d53199d47a032275acc77f55ddc6e8326654783
B-Mohammed2 <118529003+B-Mohammed2@users.noreply.github.com>
2024-02-02 17:40:25 +0000

    adding icons,and js codes

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,12 +1,43 @@
 //state
-let city="London";
+let city="oslo";
 let units="metric";
 
+
+// Define a mapping between weather conditions and icon filenames
+const weatherImg = {
+    "Clear": "clear.png",
+    "Clouds": "clouds.png",
+    "Rain": "rain.png",
+    "Snow": "snow.png",
+    "Storm": "storm.png",
+    "Wind": "wind.png",
+    "Fog": "fog.png",
+    "Mist": "mist.png",
+};
+
 //selectors
-let searchCity = document.querySelector(".weatherCity");
+let weatherCity = document.querySelector(".weatherCity");
 let dateTime = document.querySelector(".dateTime"); 
 let weatherForcast=document.querySelector(".weather");
+let weathTemp = document.querySelector(".weatherTemp");
+let weatherIcon = document.querySelector(".weatherIcon");
+let weatherMinMax=document.querySelector(".weatherMinMax");
+let feelsLike=document.querySelector(".feelsLike");
+let humidity=document.querySelector(".humidity");
+let wind=document.querySelector(".wind");
+let pressure=document.querySelector(".pressure");
 
+//search city
+
+// }
+document.querySelector(".citySearch").addEventListener('submit',e=>{
+    let search=document.querySelector(".citySearch input");
+    e.preventDefault();
+    city=search.value;
+    getWeather();
+    console.log(search.value);
+
+});
 
 // converting country code
 function convertContryCode(country){
@@ -58,13 +89,31 @@ function getWeather() {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`)
         .then(res => res.json())
         .then(data => {
-            searchCity.innerHTML=`${data.name},${convertContryCode(data.sys.country)}`;
-            dateTime.innerHTML= convertTimeStamp(data.dt,data.timezone);
-            weatherForcast.innerHTML=` <p>${data.weather[0].main}</p>`;
+            weatherCity.innerHTML = `${data.name},${convertContryCode(data.sys.country)}`;
+            dateTime.innerHTML = convertTimeStamp(data.dt, data.timezone);
+            weatherForcast.innerHTML = `<p>${data.weather[0].main}</p>`;
+            weathTemp.innerHTML = `${Math.round(data.main.temp)}&#176`;
 
+            // creating variables to find the image acordind to weather forcast.
+            let weatherCondition = data.weather[0].main;
+            if (weatherImg.hasOwnProperty(weatherCondition)) {
+                let iconFilename = weatherImg[weatherCondition];
+                weatherIcon.src = `static/img/icons/${iconFilename}`;
+            } else {
+                weatherIcon.src = `/static/img/icons/clear.png`;
+            }
+            weatherIcon.innerHTML= `<img src=${weatherIcon.src} alt="weatherIcon">`;
+
+            weatherMinMax.innerHTML=`<p>Min:${Math.round(data.main.temp_min)}&#176</p>
+            <p>Max:${Math.round(data.main.temp_max)}&#176</p>`;
+            feelsLike.innerHTML=`${Math.round(data.main.feels_like)}&#176`;
+            humidity.innerHTML=`${Math.round(data.main.humidity)}%`;
+            wind.innerHTML=`${data.wind.speed} M/S`;
+            pressure.innerHTML=`${Math.round(data.main.pressure)} hpa`;
+
+            console.log(data);
         });
 }
 
 
 document.addEventListener('DOMContentLoaded', getWeather);
-
